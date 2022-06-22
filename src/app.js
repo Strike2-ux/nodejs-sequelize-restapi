391eba650ee0dbb1686d54f354edbe4ec7d27cf5
Strike2-ux <13paullevy@gmail.com>
2022-06-22 14:40:19 -0400

    feat: agregamos acciones de sequelize

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 import express from "express";
+import projectsRoutes from "./routes/projects.routes.js";
 
 const app = express();
+
+
+//middlewares
+app.use(express.json());
+
+
+app.use(projectsRoutes);
 
 export default app;
 
