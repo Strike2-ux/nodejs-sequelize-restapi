efab7270627551c37add50e4bdb5b66da32b55f2
Strike2-ux <13paullevy@gmail.com>
2022-06-25 22:38:10 -0400

    feat: we complete the CRUD in routes and controllers

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,28 +1,59 @@
 import {Project} from '../models/Project.js';
 
-
-
 export const getProjects = async (req, res) => {
-try {    
-    throw new Error('project failed')
-    const projects = await Project.findAll();
-    res.json(projects);
-} catch (error) {
-    return res.status(500).json({ message: error.message});
-}
+
+};
+
+export const getProject = async (req, res) => {
+    try {  
+        const { id } = req.params;
+        const project = await Project.findOne({
+            where: {
+                id,
+            },
+        });
+
+        if (!project)
+            return res.status(404).json({ message: "Project does not exits" });
+            
+        res.json(project);
+        } catch (error) {
+          return res.status(500).json({ message: error.message });
+        }
 };
 
 export const createProject = async (req, res) => {
-    const {name, priority, description} = req.body;
-    try {
-        const newProject = await Project.create({
-            name,
-            description,
-            priority
-           });
+
+};
+
+export const updateProject = async (req, res) => {
+    const { id } = req.params;
+    const {name, priority, description} = req.body
+    res.send('updating project')
+
+    const project = await Project.findByPk(id)
+    project.name = name
+    project.priority = priority
+    project.description = description
+    await project.save()
+
+    res.send('updating')
+
+};
+
+export const deleteProject = async (req, res) => {
+try {
+    const { id } = req.params
     
-        res.json(newProject);
- }  catch (error) {
+    await Project.destroy({
+        where: {
+            id,
+        },
+    });
+
+    res.sendStatus(204)
+} catch (error) {
     return res.status(500).json({ message: error.message });
- }
+}
+
 };
